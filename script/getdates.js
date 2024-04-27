d9a0dcbd81d88b499e5776ed48b00f1b62c471cf
CayleighLeishman <cayleighl@byui.edu>
2024-04-27 16:51:41 -0600

    Update project

---
+++ script/getdates.js
@@ -0,0 +1,10 @@
+
+alert(document.lastModified);
+
+let olastModif = new Date(document.lastModified);
+
+let nlastModif = Date.parse(document.lastModified);
+
+
+
+//  Help from https://developer.mozilla.org/en-US/docs/Web/API/Document/lastModified
