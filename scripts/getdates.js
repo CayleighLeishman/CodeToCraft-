4e245e331e1aaec38b4299c57a1c513a144c642c
CayleighLeishman <cayleighl@byui.edu>
2024-05-11 14:48:04 -0600

    fixed lastModified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,15 +1,10 @@
 // sets the current Year
-
-let year = new date().getFullYear()
+let year = new Date().getFullYear()
 let currentYearSpan = document.querySelector('#currentYear')
 currentYearSpan.innerHTML = `${year}`
 
 
 
 // sets the last modified date
-
 let lastModifiedParagraph = document.querySelector('#lastModified')
 lastModifiedParagraph.innerHTML =`Last Modified: ${document.lastModified}`
-
-
-
