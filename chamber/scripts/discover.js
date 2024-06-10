955cf1d24394309e7ef7fa4f8d03a7f0af1dbfdc
CayleighLeishman <cayleighl@byui.edu>
2024-06-10 16:33:05 -0600

    discover page, changed image size and tried to fix image issues on chamber home page

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,29 @@
+let today = new Date();
+const DAYS_IN_MILLIS = 1000*60*60*24;
+const LAST_VISIT_KEY = "last-visit"
+
+function getMessage(){
+    let lastVisit = localStorage.getItem(LAST_VISIT_KEY)
+    let message = "Welcome! Let us know if you have any questions."
+
+    if (lastVisit == null){
+        return message
+    }
+
+    lastVisit = parseInt(lastVisit)
+    let dateDiff = today.getTime() - lastVisit 
+
+    if (dateDiff < DAYS_IN_MILLIS){
+         return "Back so soon! Awesome!"
+    }
+    if (dateDiff > DAYS_IN_MILLIS*2){
+         return  "You last visited 1 day ago"
+    }
+    let daysSinceLastVisit = Math.floor(dateDiff / DAYS_IN_MILLIS)
+
+    return `You last visited ${daysSinceLastVisit } days ago.`   
+}
+
+let message = getMessage()
+document.getElementById("discover-message").innerText = message
+localStorage.setItem(LAST_VISIT_KEY, `${today.getTime()}`)
