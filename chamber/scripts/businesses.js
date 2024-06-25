e0498bd880bcbd9d6d494fc155e80d7b53dae69a
CayleighLeishman <cayleighl@byui.edu>
2024-06-24 20:23:37 -0600

    update directory

--- chamber/scripts/businesses.js
+++ chamber/scripts/businesses.js
@@ -1,24 +1,24 @@
-const site = "https://cayleighleishman.github.io/wdd230/chamber/data/business.json"; 
-const cards = document.querySelector('#cards');
+const site = "https://cayleighleishman.github.io/wdd230/chamber/data/business.json";
+const business_cards = document.querySelector('#business_cards');
 
 const displayBusiness = (businesses) => {
     businesses.forEach((business) => {
         let newsection = document.createElement("section");
         newsection.innerHTML = `
             <h2>${business.name} ${business.label}</h2>
-            <img src="${business.imageurl}" alt="${business.name} image" loading="lazy" height="300">
+            <img src="${business.imageURL}" alt="${business.name} image" loading="lazy" height="300">
             <p>${business.address}, ${business.phone_number}</p>
-            <p>${business.websiteURL}</p>
-        `;
-        cards.append(newsection);
+            <p>${business.websiteURL}</p>`;
+        business_cards.appendChild(newsection);
     });
-}
+};
+
 async function getBusinessData(){
     try {
         const response = await fetch(site);
         if (response.ok){
             const data = await response.json();
-            displayBusiness(newsection);
+            displayBusiness(data.Businesses); // Corrected to pass data.Businesses
         } else {
             console.log("Oops! Something went wrong");
         }
