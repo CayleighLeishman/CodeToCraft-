864e8d1bf22d27bcb70f59bffd59b457d1b4b7f3
CayleighLeishman <cayleighl@byui.edu>
2024-06-27 23:35:58 -0600

    tried my hand at Week 6 project and dark mode

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -1,12 +1,38 @@
-// // sets the current Year
-// let year = new Date().getFullYear()
-// let currentYearSpan = document.querySelector('#currentYear')
-// currentYearSpan.innerHTML = `${year}`
-
-
-//professor
+// Date Stuff
 let datespan =document.querySelector('#currentYear')
 datespan.innertext = new Date().getFullYear()
 
 let lastmodifiedspan = document.querySelector('#lastModified')
 lastmodifiedspan.innerText = document.lastModified
+
+  
+  // // Navigation Hamburger
+  let menu_button = document.getElementById("menu-button")
+  let navList = document.querySelector("nav ul")
+  menu_button.addEventListener('click',() => {
+      menu_button.classList.toggle("open")
+      navList.classList.toggle("open")
+  })
+  
+  
+// darkMode 
+let darkbutton = document.getElementById("dark-mode")
+darkbutton.addEventListener("click", () =>{    
+    darkbutton.classList.toggle("dark")
+    if (darkbutton.classList.contains("dark")){
+        document.documentElement.style.setProperty('--text-color', 'white');        
+        document.documentElement.style.setProperty('--inv-text-color', 'black');        
+        document.documentElement.style.setProperty('--body-color', '478d73');    
+        document.documentElement.style.setProperty('--card-color', '#32343e');   
+        document.documentElement.style.setProperty('--grey-blue', '#404c54');    
+        document.documentElement.style.setProperty('--nav-color', '#32343e');              
+    }
+    else{
+        document.documentElement.style.setProperty('--text-color', 'black');        
+        document.documentElement.style.setProperty('--inv-text-color', 'white');        
+        document.documentElement.style.setProperty('--body-color', 'white');    
+        document.documentElement.style.setProperty('--card-color', '#A3B18A');        
+        document.documentElement.style.setProperty('--header-color', '#588157');    
+        document.documentElement.style.setProperty('--nav-color', '#3A5A40');                
+    }
+})
