b0ce5a5af0bf20fa9272639ab9236a4e13858e3b
CayleighLeishman <cayleighl@byui.edu>
2024-07-02 00:52:48 -0600

    update pages

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -26,7 +26,7 @@ darkbutton.addEventListener("click", () =>{
         document.documentElement.style.setProperty('--grey-blue', '#081924');        
         document.documentElement.style.setProperty('--blue-enough','#1b2e2e');
         document.documentElement.style.setProperty('--almost-teal','#478d73');     
-        document.documentElement.style.setProperty('--sea-green', '#0e1b0e');    
+        // document.documentElement.style.setProperty('--sea-green', '#0e1b0e');    
         document.documentElement.style.setProperty('--text-color','white');    
     }
 
@@ -36,7 +36,7 @@ darkbutton.addEventListener("click", () =>{
         document.documentElement.style.setProperty('--grey-blue', '#404c54');        
         document.documentElement.style.setProperty('--blue-enough','#446b6a'); 
         document.documentElement.style.setProperty('--almost-teal','#183228');   
-        document.documentElement.style.setProperty('--sea-green', '#5ba95d');    
+        // document.documentElement.style.setProperty('--sea-green', '#5ba95d');    
         document.documentElement.style.setProperty('--text-color','black');                  
     } 
 })
