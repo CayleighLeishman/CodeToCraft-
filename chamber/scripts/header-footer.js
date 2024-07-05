94d0e21be5caf4c4f58ad0e0331233064c92423c
CayleighLeishman <cayleighl@byui.edu>
2024-07-04 19:48:16 -0600

    updated chamber and dark mode colors

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -22,21 +22,25 @@ darkbutton.addEventListener("click", () =>{
     if (darkbutton.classList.contains("dark")){
 
         document.documentElement.style.setProperty('--background-color', '#04050a');
-        document.documentElement.style.setProperty('--primary-color', 'black');        
+        document.documentElement.style.setProperty('--primary-color', '#1b2e2e'); 
+        document.documentElement.style.setProperty('--white-text', 'white');        
         document.documentElement.style.setProperty('--grey-blue', '#081924');        
-        document.documentElement.style.setProperty('--blue-enough','#1b2e2e');
+        document.documentElement.style.setProperty('--blue-enough','#1b2e2e'); // change to a darker color
         document.documentElement.style.setProperty('--almost-teal','#478d73');     
         // document.documentElement.style.setProperty('--sea-green', '#0e1b0e');    
-        document.documentElement.style.setProperty('--text-color','white');    
+        document.documentElement.style.setProperty('--text-color','white');  
+        document.documentElement.style.setProperty('--cards-color','#062e24')  
     }
 
     else{
         document.documentElement.style.setProperty('--background-color', '#32343e');  
-        document.documentElement.style.setProperty('--primary-color', 'white');        
+        document.documentElement.style.setProperty('--primary-color', 'white');  
+        document.documentElement.style.setProperty('--white-text', 'white');       
         document.documentElement.style.setProperty('--grey-blue', '#404c54');        
         document.documentElement.style.setProperty('--blue-enough','#446b6a'); 
         document.documentElement.style.setProperty('--almost-teal','#183228');   
         // document.documentElement.style.setProperty('--sea-green', '#5ba95d');    
-        document.documentElement.style.setProperty('--text-color','black');                  
+        document.documentElement.style.setProperty('--text-color','black');   
+        docuent.documentElement.style.setProperty('--cards-color','white');           
     } 
 })
