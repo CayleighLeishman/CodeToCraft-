4bb477947b5f012cb97baa38b19af38c27102c69
CayleighLeishman <cayleighl@byui.edu>
2024-07-18 11:02:31 -0600

    update

--- a-final-project/scripts/display-products.js
+++ a-final-project/scripts/display-products.js
@@ -1,5 +1,7 @@
 // Load JSON data and do stuff
 const url = "./data/products.json";
+const productContainer = document.getElementById("products-data")
+// const productContainer2 = document.querySelector("#products-data")
 
 const displayProducts = (products) => {
     products.forEach((products) => {
@@ -9,16 +11,16 @@ const displayProducts = (products) => {
             <img src="${products.imageURL}" alt="${products.description} image" loading="lazy" height="300">
             <p>${products.price}</p>
             <p> is this a featred product? ${products.featuredProduct}</p>`;
-        products_data.appendChild(newsection);
+            productContainer.appendChild(newsection);
     });
 };
-
+// to do add to something similar to this for home page 
 async function getProductsData(){
     try {
         const response = await fetch(url);
         if (response.ok){
             const data = await response.json();
-            displayproducts(data.products); 
+            displayProducts(data.products); 
         } else {
             console.log("Oops! Something went wrong");
         }
